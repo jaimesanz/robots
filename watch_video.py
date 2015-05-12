99d880f4e0604fe0e53109846248927bed3996a1
jaimesanz <jsanz@dcc.uchile.cl>
2015-05-11 20:08:33 -0400

    Se crea archivo para ver los videos

---
+++ watch_video.py
@@ -0,0 +1,12 @@
+import cv2
+import glob, os
+os.chdir("offices_part2\office_0022a")
+for file in glob.glob("*.ppm"):
+	# print(file)
+	frame = cv2.imread(file)
+	# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# Display the resulting frame
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+print "terminado"
