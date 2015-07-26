cf47c45a4719607a468ee164ccd03c7588a6283d
jaimesanz <jsanz@dcc.uchile.cl>
2015-07-26 17:34:12 -0400

    Se agregan clasificadores svm

--- watch_video.py
+++ watch_video.py
@@ -1,6 +1,6 @@
 import cv2
 import glob, os
-os.chdir("offices_part2\office_0022a")
+os.chdir("offices_part2\office_0015")
 for file in glob.glob("*.ppm"):
 	# print(file)
 	frame = cv2.imread(file)
